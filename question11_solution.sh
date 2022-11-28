f2294eddd757f5e8d5daefdb3e5f2b7fdf0dca97
Joshua Villasis (CSUF) <42751215+JJVillasis@users.noreply.github.com>
2022-11-27 18:40:43 -0800

    Edit: Update solution

--- question11_solution.sh
+++ question11_solution.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #For loop through project directory and displays all files
-for i in `find ./project/* -type f`; do
+for i in `find ./project/* -type f -name '*.py' -o -name '*.html' -o -name '*.css'`; do
     echo $(basename $i);
 done
